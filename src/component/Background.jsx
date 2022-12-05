cd81cab1651c627661e23592e32153031ec3dc95
abhirami <abhiabhirami972@gmail.com>
2022-12-05 13:00:47 +0530

    completed twitter

---
+++ src/component/Background.jsx
@@ -0,0 +1,12 @@
+import "./background.css"
+
+function Background() {
+    return (
+        <div className="btn">
+            <img src="" alt="" />
+            <p>it just eaiser to tweet</p>
+        </div>
+    )
+}
+
+export default Background;
---
+++ src/component/MyButton.jsx
@@ -0,0 +1,13 @@
+import "./MyButton.css"
+
+function MyButton() {
+    return (
+        <div>
+            <button className="btn">following</button>
+
+        </div>
+    )
+}
+
+
+export default MyButton;
